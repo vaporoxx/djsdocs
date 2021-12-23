42c90f05e54cf8f16ee8503342ea9f0adbac2963
Vaporox <jan.zichler@gmx.de>
2021-12-23 15:11:53 +0100

    feat: replace newlines and tags

--- src/util.rs
+++ src/util.rs
@@ -1,12 +1,22 @@
 use regex::Regex;
-use std::borrow::Cow;
 use std::fmt::Display;
 use std::process;
 
-pub fn clean_description(input: &str) -> Cow<str> {
-	Regex::new(r"`([^`]*)`|\{@link ([^}]*)\}")
+pub fn clean_description(input: &str) -> String {
+	let newlines = input.replace('\n', " ");
+
+	let info = Regex::new("<info>(.*?)</info>")
 		.unwrap()
-		.replace_all(input, "$1$2")
+		.replace_all(&newlines, "Info: $1");
+
+	let warn = Regex::new("<warn>(.*?)</warn>")
+		.unwrap()
+		.replace_all(&info, "Warning: $1");
+
+	Regex::new(r"`(.*?)`|\{@link (.*?)\}|<p>(.*?)</p>")
+		.unwrap()
+		.replace_all(&warn, "$1$2$3")
+		.into_owned()
 }
 
 pub fn exit(message: impl Display) -> ! {
